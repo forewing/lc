630e27e31b85813e9253d0098644cd9170c9248e
forewing <jujianai@hotmail.com>
2021-10-20 13:20:45 -0400

    add p1216

---
+++ python/p1216.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValidPalindrome(self, s: str, k: int) -> bool:
+        n = len(s)
+        s2 = s[::-1]
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == s2[j - 1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[n][n] >= n - k
