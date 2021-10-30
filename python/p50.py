a4459b343055bdb1b606da4ec0193f29f5463fc9
forewing <jujianai@hotmail.com>
2021-10-30 18:56:37 -0400

    update p50

--- python/p50.py
+++ python/p50.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x ** n
+        return reduce(lambda s, b: s*s*x if b == "1" else s*s, bin(n)[2:], 1) ** (1 if n > 0 else -1)
