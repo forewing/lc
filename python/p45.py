714eb7c4029761e6a5bb941b5082b027bbd7abcf
forewing <13747187+forewing@users.noreply.github.com>
2021-11-15 13:46:13 -0500

    update p45

--- python/p45.py
+++ python/p45.py
@@ -1,26 +1,14 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        self.dp = [-1] * len(nums)
-        self.ok = [True] * len(nums)
-        self.dp[-1] = 0
-        self.nums = nums
-        return self.search(0)
+        n = len(nums)
+        dp = [float('inf')] * n
+        dp[0] = 0
 
-    def search(self, index):
-        if self.dp[index] != -1 or not self.ok[index]:
-            return self.dp[index]
+        tail = 1
+        for i in range(n):
+            limit = min(n, i + nums[i] + 1)
+            for j in range(tail, limit):
+                dp[j] = min(dp[j], dp[i] + 1)
+            tail = limit - 1
 
-        ok = False
-        ans = float('inf')
-        for next in range(index + 1, index + self.nums[index] + 1):
-            if next >= len(self.nums):
-                continue
-            t = self.search(next)
-            if t != -1:
-                ans = min(ans, t)
-                ok = True
-        if ok:
-            self.dp[index] = ans + 1
-        else:
-            self.ok[index] = False
-        return self.dp[index]
+        return dp[-1]
